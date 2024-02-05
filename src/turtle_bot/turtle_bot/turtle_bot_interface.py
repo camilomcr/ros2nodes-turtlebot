79884f4d04f4facb7645ade595223d057335123d
Camilo Morillo <c.morillo@uniandes.edu.co>
2024-02-04 20:15:00 -0500

    interface ready

--- src/turtle_bot/turtle_bot/turtle_bot_interface.py
+++ src/turtle_bot/turtle_bot/turtle_bot_interface.py
@@ -2,27 +2,35 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import matplotlib.pyplot as plt
 import time
-    
+import turtle
+import numpy as np
 
 class turtle_bot_interface(Node):
 
     def __init__(self):
         super().__init__("turtle_bot_interface")
-        self.x = []
-        self.y = []
-        plt.ion()
+        self.save=(input("Save route? (y/n): ")=='y')
+        if self.save:
+            self.file_name=input("file name (without .txt): ")
+            self.file_path="src/turtle_bot/resource/"+self.file_name+".txt"
+        self.x = 0
+        self.y = 0
+        self.s = turtle.getscreen()
+        self.t = turtle.Turtle()
         self.get_logger().info("turtle bot interface started")
         self.turtle_bot_suscriber = self.create_subscription(Twist, "/turtlebot_position", self.position_callback, 10)
         
 
     def position_callback(self, msg: Twist):
-        self.x.append(msg.linear.x)
-        self.y.append(msg.linear.y)
-        plt.plot(self.x,self.y, linestyle='-')
+        self.x = msg.linear.x
+        self.y = msg.linear.y
+        self.t.goto(int(100*self.x),int(100*self.y))
         self.get_logger().info(f'moving to ({msg.linear.x}, {msg.linear.y})')
-        time.sleep(2)
+        if self.save:
+            with open(self.file_path, 'a') as file:
+                file.write(f'{self.x},{self.y}\n')
+        time.sleep(0.5)
 
 def main(args=None):
     rclpy.init(args=args)
