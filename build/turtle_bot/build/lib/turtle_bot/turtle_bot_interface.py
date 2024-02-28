46cdb78d64aa9d828bdb72ecbdb32d1ce7bcfc67
Camilo <c.morillo@uniandes.edu.co>
2024-02-28 10:23:09 -0500

    update interface

--- build/turtle_bot/build/lib/turtle_bot/turtle_bot_interface.py
+++ build/turtle_bot/build/lib/turtle_bot/turtle_bot_interface.py
@@ -27,12 +27,12 @@ class turtle_bot_interface(Node):
         self.pos = False
         self.fig = Figure(figsize=(5, 4), dpi=100)
         self.plot_frame = tk.Frame(gui)
-        self.plot_frame.pack(side=tk.RIGHT,fill=tk.BOTH, expand = True)
+        self.plot_frame.grid(row=0, column=2, rowspan=5)
         self.subplot= self.fig.add_subplot()
         self.subplot.set_xlim(-250, 250)
         self.subplot.set_ylim(-250, 250)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)
-        self.canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack()
         self.turtle_bot_pos_suscriber = self.create_subscription(Twist, "/turtlebot_position", self.position_callback, 10)
         self.get_logger().info("turtle bot position drawer started")
 
@@ -83,27 +83,37 @@ class turtle_bot_interface(Node):
         
 
     def follow_path(self, file_name: str):
-        self.req.map_url = file_name
+        self.req.map_url =  file_name
         self.future = self.client.call(self.req)
         return self.future.result()
+    
+    def stop_save(self):
+        self.destroy_subscription(self.turtle_bot_vel_suscriber)
 
 class gui_class():
 
     def __init__(self, args):
+        threading.Thread(target=self.start_node, args=(args,)).start()
         self.gui = tk.Tk()
         self.gui.title('Turtle bot interface')
+        self.saving=False
         self.button_position = tk.Button(self.gui, text='Position tracker', width=25, command=self.gui_position)
         self.save_text_box = tk.Text(self.gui,height= 1, width=20)
-        self.button_save = tk.Button(self.gui, text='Save position', width=25, command=self.gui_save)
+        self.button_save = tk.Button(self.gui, text='Save position', width=15, command=self.gui_save)
+        self.button_stop_save = tk.Button(self.gui, text='Stop saving position', width=15, command=self.gui_stop_save)
         self.follow_text_box = tk.Text(self.gui,height= 1, width=20)
         self.button_follow = tk.Button(self.gui, text='Follow path', width=25, command=self.gui_follow)
-        self.button_position.pack(pady=10)
-        self.save_text_box.pack(pady=10)
-        self.button_save.pack(pady=10)
-        self.follow_text_box.pack(pady=10)
-        self.button_follow.pack(pady=10)
+
+
+        self.button_position.grid(row = 0, column = 0, columnspan=2, pady=10)
+        tk.Label(self.gui, text="Save file name").grid(row=1, column=0, pady=10)
+        self.save_text_box.grid(row = 1, column = 1,pady=10)
+        self.button_save.grid(row = 2, column = 0, pady=10)
+        self.button_stop_save.grid(row = 2, column = 1, pady=10)
+        tk.Label(self.gui, text="Path file name").grid(row=3, column=0, pady=10)
+        self.follow_text_box.grid(row = 3, column = 1, pady=10)
+        self.button_follow.grid(row = 4, column = 0, columnspan=2, pady=10)
         self.open_pos = False
-        threading.Thread(target=self.start_node, args=(args,)).start()
         self.gui.mainloop()
     
     def start_node(self, args):
@@ -114,21 +124,32 @@ class gui_class():
     
     def gui_position(self):
         self.open_pos= not self.open_pos
-        self.node.position_loop(self.gui, self.open_pos)
+        threading.Thread(target=self.node.position_loop, args=(self.gui,self.open_pos,)).start()
 
     def gui_save(self):
+        if self.saving:
+            messagebox.showwarning("Warning","A route is being saved")
+            return
         text = self.save_text_box.get(1.0, "end-1c")
         if text=='':
             messagebox.showwarning("Warning","Invalid file name")
             return
-        self.node.save_loop(text)
+        self.saving = True
+        threading.Thread(target=self.node.save_loop, args=(text,)).start()
+    
+    def gui_stop_save(self):
+        if not self.saving:
+            messagebox.showwarning("Warning","There is no route being saved")
+            return
+        self.saving = False
+        threading.Thread(target=self.node.stop_save).start()
 
     def gui_follow(self):
         text = self.follow_text_box.get(1.0, "end-1c")
         if not os.path.exists("src/turtle_bot/resource/"+text+".txt"):
             messagebox.showwarning("Warning","File not found")
             return
-        self.node.player_loop()
+        threading.Thread(target=self.node.player_loop, args=(text,)).start()
 
 
 def main(args=None):
