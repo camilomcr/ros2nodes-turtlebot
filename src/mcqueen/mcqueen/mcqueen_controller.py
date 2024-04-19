505b8d5d9506c0ceaf8414b619344ca168f62c5a
Camilo <c.morillo@uniandes.edu.co>
2024-04-19 01:42:06 -0500

    update controller

--- src/mcqueen/mcqueen/mcqueen_controller.py
+++ src/mcqueen/mcqueen/mcqueen_controller.py
@@ -9,15 +9,17 @@ class mcqueen_controller(Node):
     def __init__(self):
         super().__init__("mcqueen_controller")
         try:
-            self.ser = serial.Serial('dev/ttyACM0',9600)
+            self.ser = serial.Serial('/dev/ttyACM0',9600)
+            self.ser.reset_input_buffer()
         except:
             self.get_logger().info("Serial not found")
         self.mcqueen_motor_subscription = self.create_subscription(Twist, "/mcqueen_cmdVel", self.motor_callback, 10)
         self.get_logger().info(f"mcqueen controller started")
 
     def motor_callback(self, msg: Twist):
-        self.get_logger().info(f"{msg.linear.x}, {msg.linear.y}, {msg.linear.z}, {msg.angular.x}, {msg.angular.y}, {msg.angular.z}")
-        self.ser.write(f"{msg.linear.x}, {msg.linear.y}, {msg.linear.z}, {msg.angular.x}, {msg.angular.y}, {msg.angular.z}\n")
+        self.ser.write(f"{msg.linear.x}, {msg.linear.y}, {msg.linear.z}, {msg.angular.x}, {msg.angular.y}, {msg.angular.z}\n".encode('utf-8'))
+        line = self.ser.readline().decode('utf-8').rstrip()
+        self.get_logger().info(f"Received {line}")
 
 
 def main(args=None):
